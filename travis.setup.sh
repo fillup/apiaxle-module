e7957432b2e6261aae42dde0c7497843e2827bd4
Phillip Shipley <phillip_shipley@sil.org>
2013-06-17 18:57:20 -0400

    moving curl calls to bash script

---
+++ travis.setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -X POST -H 'Content-type:\ application/json' 'http://localhost:8000/v1/api/apiaxle' -d '{"endPoint":"localhost:8000"}'
+curl -X POST -H 'Content-type:\ application/json' 'http://localhost:8000/v1/key/apiaxle-travis-ci-key' -d '{"sharedSecret":"apiaxle-travis-ci-secret"}'
+curl -X PUT 'http://localhost:8000/v1/api/apiaxle/linkkey/apiaxle-travis-ci-key'
+cd config/
+ln -s config.travis.php config.local.php
+exit 0
