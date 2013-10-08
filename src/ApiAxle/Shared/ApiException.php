9ad25d05b03893e8758f699d77ba4819e0be8124
Phillip Shipley <phillip_shipley@sil.org>
2013-10-08 10:45:53 -0400

    forgot to update license in each file

--- src/ApiAxle/Shared/ApiException.php
+++ src/ApiAxle/Shared/ApiException.php
@@ -3,7 +3,7 @@
  * ApiAxle (https://github.com/fillup/apiaxle-module)
  *
  * @link      https://github.com/fillup/apiaxle-module for the canonical source repository
- * @license   GPLv2+
+ * @license   MIT
  */
 namespace ApiAxle\Shared;
 
