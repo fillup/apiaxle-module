d48eb9f84a0c69aefd6e17600dd88fa78f47c158
Phillip Shipley <phillip_shipley@sil.org>
2013-10-23 11:23:34 -0400

    updated travis config file

--- config/config.travis.php
+++ config/config.travis.php
@@ -3,5 +3,9 @@
 return array(
     'endpoint' => 'http://apiaxle.api.local:3000/v1',
     'key' => 'apiaxle-travis-ci-key',
-    'secret' => 'apiaxle-travis-ci-secret'
+    'secret' => 'apiaxle-travis-ci-secret',
+    'ssl_verifypeer' => true,
+    'proxy_enable' => false,
+    'proxy_host' => '127.0.0.1',
+    'proxy_port' => '8888',
 );
